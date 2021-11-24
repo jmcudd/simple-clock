46073c0839de842b6691a8757f62afd0f6bd9595
John Cudd <john.cudd@deciphernow.com>
2021-11-24 11:52:04 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<svelte:options tag="simple-clock" />
+<script>
+	import {onMount} from "svelte";
+	export let size=15;
+	let now = new Date();
+	let out=`00:00:00`;
+	function dd(num){
+		return num<10 ? `0${num}`:num;
+	}
+	onMount(()=>{
+		setInterval(function(){
+			now = new Date();
+			out=`${dd(now.getHours())}:\
+            ${dd(now.getMinutes())}:\
+            ${dd(now.getSeconds())}`;
+	  }, 500);
+	})
+</script>
+<div class="clock" style="font-size:{size}px;">{out}</div>
+<style>
+  .clock {
+    border: 5px solid #d44400;
+    border-radius: 10px;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+  }
+</style>
